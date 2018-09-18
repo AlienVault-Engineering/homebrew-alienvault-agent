b56a801ba23016c673e388136ec17cf789925601
Rusty Brooks <rbrooks@alienvault.com>
2018-09-17 20:24:14 -0500

    Adding brew formula for version 1.0.1

--- alienvault-agent.rb
+++ alienvault-agent.rb
@@ -2,7 +2,7 @@ class AlienvaultAgent < Formula
   desc "AlienVault Agent"
   homepage "https://www.alienvault.com/"
   url "https://s3-us-west-2.amazonaws.com/ci-otxb-portal-osquery/repo/osx/alienvault-agent-1.0.1.tar.gz"
-  sha256 "47d0c629ca0bc8547808a9c541543d06b54ad9e3128c38c63bdc12c90270899f"
+  sha256 "8dac11bbd97b78d7ae2a6c96b41f4700f4971f9e80645fd32cc81a37abf599b4"
   version "1.0.1"
   plist_options :startup => true
 
